0282e470cc441584d7b20d59c07cbbed8c30be0d
n ashok <ashokn1290@gmail.com>
2022-07-25 15:56:14 +0530

    ashok

--- practice-project16/src/ExceptionHandling/MyException.java
+++ practice-project16/src/ExceptionHandling/MyException.java
@@ -1,6 +1,6 @@
 package ExceptionHandling;
 
-class MyException extends Exception{
+ class MyException extends Exception{
 	   String str1;
 	   MyException(String str2) {
 		str1=str2;
@@ -11,15 +11,3 @@ class MyException extends Exception{
 	}
 
 
-class Example1{
-	   public static void main(String args[]){
-		try{
-			System.out.println("Starting of try block");
-			throw new MyException("This is My error Message");
-		}
-		catch(MyException exp){
-			System.out.println("Catch Block") ;
-			System.out.println(exp) ;
-		}
-	   }
-	}
---
+++ practice-project20/src/ArrayRotation/arrayrotation.java
@@ -0,0 +1,29 @@
+package ArrayRotation;
+
+class RA {
+	public void rotate(int[] number, int k) {
+		if (k > number.length)
+			k = k % number.length;
+		int[] result = new int[number.length];
+		for (int i = 0; i < k; i++) {
+			result[i] = number[number.length - k + i];
+		}
+		int j = 0;
+		for (int i = k; i < number.length; i++) {
+			result[i] = number[j];
+			j++;
+		}
+		System.arraycopy(result, 0, number, 0, number.length);
+	}
+}
+
+public class arrayrotation {
+	public static void main(String[] args) {
+		RA r = new RA();
+		int arr[] = { 1, 9, 4, 5, 3, 7, 3, 0 };
+		r.rotate(arr, 5);
+		for (int i = 0; i < arr.length; i++) {
+			System.out.print(arr[i] + " ");
+		}
+	}
+}
